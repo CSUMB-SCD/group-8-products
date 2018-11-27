8b3a82ea2ffa65b15b1d21d5433d8d90a52f0340
Alexander Paulsell <alexander.dean.paulsell@gmail.com>
2018-11-26 21:20:04 -0800

    shity version control

--- src/main/java/edu/csumb/cst438/productservice/data/ProductDbClient.java
+++ src/main/java/edu/csumb/cst438/productservice/data/ProductDbClient.java
@@ -4,19 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.RestTemplate;
 
 import edu.csumb.cst438.productservice.api.products.Product;
 
 @Repository
 public class ProductDbClient {
+
+    @Autowired
+    ProductRepository repository;
     
     public List<Product> getAll(){
-        final String uri = "https://proj-products.herokuapp.com/Product";
-
-        RestTemplate restTemplate = new RestTemplate();
-        List<Product> result = restTemplate.getForObject(uri, List.class);
-    
-        return result;
+        return repository.findAll();
     }
 }
